24b46736734ef929f361db92d277ccf5719c29f5
Armin <64209484+DasAndereHash@users.noreply.github.com>
2023-09-18 15:59:42 +0200

    configured rpki for r1

--- r1/frr_start.sh
+++ r1/frr_start.sh
@@ -1,4 +1,5 @@
-echo "1.1.10.11       routinator.cache" | sudo tee -a /etc/hosts
+#echo "1.1.10.11       routinator.cache" | sudo tee -a /etc/hosts
+#/rsyslog-bin/rsyslogd -f /etc/rsyslog.conf -M /rsyslog-bin
 chown -R frr.frr /etc/frr
 service frr start
 sleep infinity
